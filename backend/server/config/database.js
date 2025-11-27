7aa18c918fbf73d6f9786de32c24aa01c6e3d1a0
jansq. <aditdiscord17@gmail.com>
2025-11-27 20:40:11 +0700

    Update database.js

--- backend/server/config/database.js
+++ backend/server/config/database.js
@@ -4,14 +4,24 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    
+    // --- BAGIAN INI YANG PENTING ---
+    // Kode ini akan mencari variable 'DB_PORT' di Vercel (yang isinya 13781)
+    // Jika tidak ketemu (misal di laptop), dia pakai 3306
+    port: process.env.DB_PORT || 3306, 
+    
+    // Wajib tambah ini untuk Aiven (SSL)
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
     if (err) {
         console.error('Error Koneksi Database:', err);
     } else {
-        console.log('Database Terkoneksi (Config)!');
+        console.log('Database Terkoneksi!');
     }
 });
 
